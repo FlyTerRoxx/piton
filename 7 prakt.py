70be027ca172e2f7504712bc39ab1c570e21c895
FlyTerRoxx <121129262+FlyTerRoxx@users.noreply.github.com>
2022-12-20 22:53:28 +0300

    Add files via upload

---
+++ 7 prakt.py
@@ -0,0 +1,14 @@
+print(len(set(str(input()))))
+#количество уникальных символов
+
+
+string_1 = (set(str(input())))
+string_2 = (set(str(input())))
+print(string_1 - string_2)
+#элементы содержащиеся только в 1 строке
+
+
+string_1 = (set((input().split())))
+string_2 = (set((input().split())))
+print(string_1 & string_2)
+#числа, которые встречаются в обеих
